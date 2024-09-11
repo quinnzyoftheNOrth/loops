a1bd4339f5d84dabd32de18247c9085a95785b62
quinnzyoftheNOrth <getreadytogetkaidfiyahhhh@gmail.com>
2024-09-11 11:12:51 +0200

    loops

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+{int tal=5; 
+while (tal !=0)
+{
+    Console.Write("skriv ett tal: ");
+    tal= Convert.ToInt32(Console.ReadLine());
+    if(tal % 2 == 0)
+       Console.WriteLine("talet är jämnt");
+    else
+       Console.WriteLine("talet är udda");
+}
+}
